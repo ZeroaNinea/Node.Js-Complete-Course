b70ab203bab6b095b70665f92c42770ef920da67
Catgirl <zeroaninea@gmail.com>
2024-11-14 22:54:52 +0400

    third lesson finished

--- classcentral-jwt-course/index.ts
+++ classcentral-jwt-course/index.ts
@@ -1,7 +1,13 @@
+import { Request, Response } from "express";
+
 const express = require("express");
 
 const app = express();
 
+app.get("/", (req: Request, res: Response) => {
+  res.send(`<h1>Hi, I'm working!</h1>`);
+});
+
 app.listen(5000, () => {
-  console.log("Now running on port 5000");
+  console.log("Now the server is running on the port 5000...");
 });
