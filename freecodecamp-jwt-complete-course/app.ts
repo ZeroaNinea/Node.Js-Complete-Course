93905fc8c7e099a9c757c3ea07937eb0c72fc1aa
Catgirl <zeroaninea@gmail.com>
2024-11-27 19:35:28 +0400

    wirking with express

--- freecodecamp-jwt-complete-course/app.ts
+++ freecodecamp-jwt-complete-course/app.ts
@@ -1,5 +1,17 @@
-const express = require("express");
+import express from "express";
+import { Request, Response, NextFunction } from "express";
 
 const app = express();
+
+app.use(middleware);
+
+function middleware(req: Request, res: Response, next: NextFunction) {
+  console.log("Hello from the middleware!");
+  next();
+}
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send(`<h1>Hello, World!</h1>`);
+});
 
 app.listen(3000);
