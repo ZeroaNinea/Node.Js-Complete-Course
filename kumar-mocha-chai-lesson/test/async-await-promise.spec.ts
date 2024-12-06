e3d6059927db1142bcbfbb61d0140f2f391bc6f4
Catgirl <zeroaninea@gmail.com>
2024-12-07 00:48:18 +0400

    asynchronous testing completed

---
+++ kumar-mocha-chai-lesson/test/async-await-promise.spec.ts
@@ -0,0 +1,22 @@
+import { expect } from "chai";
+import Calculator from "../src/calculator";
+
+describe.only("ASYNC AWAIT AND PROMISE TEST SUITE", () => {
+  let calc: Calculator;
+
+  it("should work with async and await", async () => {
+    calc = new Calculator();
+
+    const result = await calc.asyncFunctionPromise();
+
+    expect(result).to.equal(4);
+  });
+
+  it("should work with async and await", async () => {
+    calc = new Calculator();
+
+    calc.asyncFunctionPromise().then((result) => {
+      expect(result).to.equal(4);
+    });
+  });
+});
