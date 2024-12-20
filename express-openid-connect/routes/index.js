171022e4db8a2f0288879f415cd97d8f4ac2f1bc
Catgirl <zeroaninea@gmail.com>
2024-12-21 00:01:12 +0400

    the error with logout fixed

--- express-openid-connect/routes/index.js
+++ express-openid-connect/routes/index.js
@@ -1,18 +1,24 @@
-var router = require('express').Router();
-const { requiresAuth } = require('express-openid-connect');
+var router = require("express").Router();
+const { requiresAuth } = require("express-openid-connect");
 
-router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'Auth0 Webapp sample Nodejs',
-    isAuthenticated: req.oidc.isAuthenticated()
+require("dotenv").config();
+
+router.get("/", function (req, res, next) {
+  res.render("index", {
+    title: "Auth0 Webapp sample Nodejs",
+    isAuthenticated: req.oidc.isAuthenticated(),
   });
 });
 
-router.get('/profile', requiresAuth(), function (req, res, next) {
-  res.render('profile', {
+router.get("/profile", requiresAuth(), function (req, res, next) {
+  res.render("profile", {
     userProfile: JSON.stringify(req.oidc.user, null, 2),
-    title: 'Profile page'
+    title: "Profile page",
   });
+});
+
+router.get("/logout", (req, res) => {
+  res.oidc.logout({ returnTo: process.env.BASE_URL });
 });
 
 module.exports = router;
