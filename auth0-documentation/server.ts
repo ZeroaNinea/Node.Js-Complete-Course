1f0a62e23910cd2b27b0e3b6bfbe7a305cfc7968
Catgirl <zeroaninea@gmail.com>
2024-12-25 03:10:52 +0400

    custom api fixed

--- auth0-documentation/server.ts
+++ auth0-documentation/server.ts
@@ -8,14 +8,23 @@ dotenv.config();
 
 const app = express();
 
-const { BASE_URL, CLIENT_ID, CLIENT_SECRET, HOST, FAKESTORE_API } = process.env;
+const {
+  ISSUER_BASE_URL,
+  CLIENT_ID,
+  CLIENT_SECRET,
+  HOST,
+  FAKESTORE_API,
+  GIRLIEST_API,
+  GIRLIEST_API_ACCESS_TOKEN,
+} = process.env;
 const PORT = parseInt(process.env.PORT!) || 5000;
 
-var options = {
+// Test request to my own API.
+const options = {
   method: "GET",
-  url: FAKESTORE_API,
+  url: `http://${HOST}:${PORT}/api/private`,
   headers: {
-    authorization: `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImtfNjItWVVFZzlGQWozYVNrN2taRiJ9.eyJpc3MiOiJodHRwczovL2Rldi04Zmx0dzVsZXlkZjZyNzJ5LnVzLmF1dGgwLmNvbS8iLCJzdWIiOiJvR1lPSUhlV3FJNUVWejgwS084djJEcXYzejdFaUVQekBjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9mYWtlc3RvcmVhcGkuY29tL3Byb2R1Y3RzIiwiaWF0IjoxNzM1MDc3NzYwLCJleHAiOjE3MzUxNjQxNjAsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyIsImF6cCI6Im9HWU9JSGVXcUk1RVZ6ODBLTzh2MkRxdjN6N0VpRVB6In0.ShtDpR0GL72PhDDnoBsVu9xU86KgIYqNYc70n-rzh4Gx52m8iTD-RKPr2_x9laqrMk3NHnuD_l_iXTY8iSUS_pwod99K_kCyYxDVJiNwXu4Uz1I5GceVeYtzZzT4UIkHIlpD15m4gie8WGcsCNzHE7b6ZGYqriu6ebXkyB3kzVY8Mkn2MNoA-FMVFyM0inTvGShzNqqJ0RtzsfmIxh0LsS_dovjMmJS0T3VGHPY5R43Nlhhe9tuBveUzATSuRWeMOXCDmMFQQzGvHDzRwDoyYRCqFWt7M256Bi4c1dlFN3v_Jret9FlE7Hl4yh63PNRJ27iTar7vy6cqpPvABQf4cg`,
+    authorization: `Bearer ${GIRLIEST_API_ACCESS_TOKEN}`,
   },
 };
 
@@ -29,8 +38,9 @@ axios
   });
 
 const checkJwt = auth({
-  audience: FAKESTORE_API,
-  issuerBaseURL: BASE_URL,
+  audience: GIRLIEST_API,
+  issuerBaseURL: ISSUER_BASE_URL,
+  tokenSigningAlg: "RS256",
 });
 
 // This route doesn't need authentication.
