3bbc95693a0de3f29ec5eeac1b61cad37155309e
Catgirl <zeroaninea@gmail.com>
2025-01-03 01:07:26 +0400

    getdatatest command created

--- cypress-course/cypress/e2e/fundamentals.cy.js
+++ cypress-course/cypress/e2e/fundamentals.cy.js
@@ -1,5 +1,32 @@
 describe("Fundamentals test", () => {
-  it("passes", () => {
-    cy.visit("https://example.cypress.io");
+  beforeEach(() => {
+    cy.visit("/fundamentals");
+  });
+
+  it("Contains correct header text", () => {
+    // cy.get('[data-test="fundamentals-header"]').contains(
+    //   /Testing Fundamentals/i
+    // );
+    // cy.get('[data-test="fundamentals-header"]').should(
+    //   "contain.text",
+    //   "Testing Fundamentals"
+    // );
+
+    // Use the custom commad.
+    cy.getDataTest("fundamentals-header").contains(/Testing Fundamentals/i);
+    cy.getDataTest("fundamentals-header").should(
+      "contain.text",
+      "Testing Fundamentals"
+    );
+  });
+
+  it("Accordion works correctly", () => {
+    cy.contains(/Your tests will exist in a describe block/i).should(
+      "not.be.visible"
+    );
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click().pause();
+    cy.contains(/Your tests will exist in a describe block/i).should(
+      "be.visible"
+    );
   });
 });
