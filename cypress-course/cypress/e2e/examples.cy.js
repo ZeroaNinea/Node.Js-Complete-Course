9352e46593e0fd93f29166d7cbc8de5f510a87bf
Catgirl <zeroaninea@gmail.com>
2025-01-04 04:34:00 +0400

    trying to fix the error with imports

--- cypress-course/cypress/e2e/examples.cy.js
+++ cypress-course/cypress/e2e/examples.cy.js
@@ -5,7 +5,7 @@ describe("Various examples", () => {
 
   it("multi-page testing", () => {
     cy.getDataTest("nav-why-cypress").click();
-    cy.location("pathname").should("equal", "/");
+    cy.location("pathname").should("equal", "/"); // The path should be equal `/` when clicking on the `nav-why-cypress` link.
 
     cy.getDataTest("nav-overview").click();
     cy.location("pathname").should("equal", "/overview");
@@ -15,5 +15,70 @@ describe("Various examples", () => {
 
     cy.getDataTest("nav-examples").click();
     cy.location("pathname").should("equal", "/examples");
+  });
+
+  it("intercepts", () => {
+    cy.intercept("POST", "http://localhost:3000/examples", {
+      // Sends an HTTP request to the server.
+      // body: {
+      //   // Stubbed response from the Cypress.
+      //   message: "successfully intercepted request",
+      // },
+
+      // Return the fixture from the `fixtures` folder as mock data.
+      fixture: "example.json",
+    });
+
+    cy.getDataTest("post-button").click();
+  });
+
+  it("grudges", () => {
+    cy.contains(/add some grudges/i);
+
+    cy.getDataTest("grudge-list").within(() => {
+      cy.get("li").should("have.length", 0);
+    });
+
+    cy.getDataTest("clear-button").should("not.exist");
+
+    cy.getDataTest("grudge-list-title").should("have.text", "Add Some Grudges");
+
+    cy.getDataTest("grudge-input").within(() => {
+      cy.get("input").type("I hate fucking developers!");
+    });
+    cy.getDataTest("add-grudge-button").click();
+
+    cy.getDataTest("grudge-list").within(() => {
+      cy.get("li").should("have.length", 1);
+    });
+
+    cy.getDataTest("grudge-list-title").should("have.text", "Grudges");
+
+    cy.getDataTest("grudge-input").within(() => {
+      cy.get("input").type("number 2");
+    });
+    cy.getDataTest("add-grudge-button").click();
+
+    cy.getDataTest("grudge-list").within(() => {
+      cy.get("li").should("have.length", 2);
+      cy.get("li").its(0).should("contains.text", "I hate fucking developers!");
+    });
+
+    cy.getDataTest("grudge-list").within(() => {
+      cy.get("li")
+        .its(0)
+        .within(() => {
+          cy.get("button").click();
+        });
+    });
+
+    cy.getDataTest("grudge-list").within(() => {
+      cy.get("li").should("have.length", 1);
+    });
+
+    cy.getDataTest("clear-button").click();
+    cy.getDataTest("grudge-list").within(() => {
+      cy.get("li").should("have.length", 0);
+    });
   });
 });
