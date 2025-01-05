ee2e607ff75125199fcacb4823bad478d089ff29
Catgirl <zeroaninea@gmail.com>
2025-01-05 16:58:51 +0400

    lesson 4 completed

--- cypress-realworld-testing-course-app/cypress/e2e/home.cy.ts
+++ cypress-realworld-testing-course-app/cypress/e2e/home.cy.ts
@@ -3,13 +3,23 @@ describe("home page", () => {
     cy.visit("http://localhost:3000");
   });
 
-  it("the h1 contains the correct text", () => {
-    cy.get("[data-test='hero-heading']")
-      .should("exist")
-      .contains("Testing Next.js Applications with Cypress");
+  context("hero section", () => {
+    // The `context` function creates a drop-down menu for a group of cases.
+    it("the h1 contains the correct text", () => {
+      cy.get("h1")
+        .should("exist")
+        .contains("Testing Next.js Applications with Cypress");
+    });
+
+    it("the features on the homepage are correct", () => {
+      cy.get("dt").eq(0).contains("4 Courses");
+    });
   });
 
-  it.only("the features on the homepage are correct", () => {
-    cy.get("dt").eq(0).contains("4 Courses");
+  context("Courses section", () => {
+    it.only("Course: Testing Your First Next.js Application", () => {
+      cy.getByData("course-0").find("a").eq(3).click();
+      cy.location("pathname").should("eq", "/testing-your-first-application");
+    });
   });
 });
