482b45f501bd8f1b4e1a626ed10745d3f3c8b651
Catgirl <zeroaninea@gmail.com>
2025-01-26 16:41:26 +0400

    sync-io tests completed

--- node-clinic-doctor-examples/sync-io/index.js
+++ node-clinic-doctor-examples/sync-io/index.js
@@ -1,28 +1,32 @@
-'use strict'
+"use strict";
 
-const restify = require('restify')
-const fs = require('fs')
-const path = require('path')
-const tmp = path.join(__dirname, 'tmp')
-const server = restify.createServer()
+const restify = require("restify");
+const fs = require("fs");
+const path = require("path");
+const tmp = path.join(__dirname, "tmp");
+const server = restify.createServer();
 
-server.get('/', function (req, res, next) {
-  sleep(10) // mimic sync I/O by sleeping 10ms sync
-  res.send({})
-  next()
-})
+server.get("/", function (req, res, next) {
+  // sleep(10) // mimic sync I/O by sleeping 10ms sync
+  setTimeout(() => {
+    res.send({});
+    next();
+  }, 1);
+});
 
-server.listen(3000)
+server.listen(3000);
 
-process.on('SIGINT', function () {
-  console.error('Caught SIGINT, shutting down.')
-  try { fs.unlinkSync(tmp) } catch (e) {}
-  server.close()
-})
+process.on("SIGINT", function () {
+  console.error("Caught SIGINT, shutting down.");
+  try {
+    fs.unlinkSync(tmp);
+  } catch (e) {}
+  server.close();
+});
 
-function sleep (ms) {
-  var now = Date.now()
-  while (Date.now() < now + ms) {
-    fs.closeSync(fs.openSync(tmp, 'a'))
-  }
-}
+// function sleep (ms) {
+//   var now = Date.now()
+//   while (Date.now() < now + ms) {
+//     fs.closeSync(fs.openSync(tmp, 'a'))
+//   }
+// }
