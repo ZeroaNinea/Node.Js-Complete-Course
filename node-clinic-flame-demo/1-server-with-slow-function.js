f05b719ef9d59ba3aaa34e9a3b277adaf4a2596d
Catgirl <zeroaninea@gmail.com>
2025-01-26 19:05:20 +0400

    the ititPayload function improved in 1-server-with-slow-function.js

--- node-clinic-flame-demo/1-server-with-slow-function.js
+++ node-clinic-flame-demo/1-server-with-slow-function.js
@@ -11,19 +11,32 @@ app.get("/", (req, res) => {
 
 app.listen(3000, () => console.log("Server is running on port: 3000..."));
 
+// function initPayload(idSize = 20) {
+//   if (idSize < 6) throw Error("idSize must be greater than 5");
+//   return function payload() {
+//     let chars = "";
+//     let n = idSize;
+//     const date = Date.now();
+//     const radix = 36;
+//     n *= n * idSize;
+//     while (n--) {
+//       const num = date + n;
+//       chars += num.toString(radix).toUpperCase();
+//     }
+//     const id = chars.slice(-idSize);
+//     return { date, id };
+//   };
+// }
+
 function initPayload(idSize = 20) {
   if (idSize < 6) throw Error("idSize must be greater than 5");
+  const max = 2147483647;
+  var count = 0;
   return function payload() {
-    let chars = "";
-    let n = idSize;
+    count = (count + 1) % max;
     const date = Date.now();
-    const radix = 36;
-    n *= n * idSize;
-    while (n--) {
-      const num = date + n;
-      chars += num.toString(radix).toUpperCase();
-    }
-    const id = chars.slice(-idSize);
+    const chars = count.toString(36).toUpperCase();
+    const id = "0".repeat(idSize - chars.length) + chars;
     return { date, id };
   };
 }
