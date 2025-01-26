a7bc10b6eeec741d889b37ccf92a3fe00ac828cf
Catgirl <zeroaninea@gmail.com>
2025-01-26 16:04:00 +0400

    slow-gc tests completed

--- node-clinic-doctor-examples/slow-gc/index.js
+++ node-clinic-doctor-examples/slow-gc/index.js
@@ -1,61 +1,61 @@
-'use strict'
+"use strict";
 
-const restify = require('restify')
-const server = restify.createServer()
+const restify = require("restify");
+const server = restify.createServer();
 
-function createLargeLinkStructure (tail, item, repeats, callback) {
+function createLargeLinkStructure(tail, item, repeats, callback) {
   setImmediate(function () {
     if (repeats > 0) {
-      const next = JSON.parse(item)
-      tail.next = next
-      createLargeLinkStructure(next, item, repeats - 1, callback)
+      const next = JSON.parse(item);
+      tail.next = next;
+      createLargeLinkStructure(next, item, repeats - 1, callback);
     } else {
-      callback(null, tail)
+      callback(null, tail);
     }
-  })
+  });
 }
 
-function getLargeData (data, callback) {
-  const item = JSON.stringify(data)
-  const first = JSON.parse(item)
+function getLargeData(data, callback) {
+  const item = JSON.stringify(data);
+  const first = JSON.parse(item);
   createLargeLinkStructure(first, item, 512, function (err, last) {
-    if (err) return callback(err)
+    if (err) return callback(err);
 
     // make data circular
-    last.next = first
-    callback(null, first)
-  })
+    last.next = first;
+    callback(null, first);
+  });
 }
 
-function getVersion (bigdata, callback) {
+function getVersion(bigdata, callback) {
   setTimeout(function () {
-    callback(null, bigdata.version)
-  }, 1000)
+    callback(null, bigdata.version);
+  }, 1); // Solusion: 1000 was decreased to 1.
 }
 
-function processRequest (data, callback) {
+function processRequest(data, callback) {
   getLargeData(data, function (err, bigdata) {
-    if (err) return callback(err)
+    if (err) return callback(err);
 
     getVersion(bigdata, function (err, version) {
-      if (err) return callback(err)
+      if (err) return callback(err);
 
-      callback(null, { version })
-    })
-  })
+      callback(null, { version });
+    });
+  });
 }
 
-server.get('/', function (req, res, next) {
-  processRequest(require('../package.json'), function (err, output) {
-    if (err) return next(err)
-    res.send(output)
-    next()
-  })
-})
+server.get("/", function (req, res, next) {
+  processRequest(require("../package.json"), function (err, output) {
+    if (err) return next(err);
+    res.send(output);
+    next();
+  });
+});
 
-server.listen(3000)
+server.listen(3000);
 
-process.on('SIGINT', function () {
-  console.error('Caught SIGINT, shutting down.')
-  server.close()
-})
+process.on("SIGINT", function () {
+  console.error("Caught SIGINT, shutting down.");
+  server.close();
+});
