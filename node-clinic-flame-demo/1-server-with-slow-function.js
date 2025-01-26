d45f8d58d498c73ee1d0705e14e22b2cac732c79
Catgirl <zeroaninea@gmail.com>
2025-01-26 17:32:14 +0400

    server is running added in the 1-server-with-slow-function.js

--- node-clinic-flame-demo/1-server-with-slow-function.js
+++ node-clinic-flame-demo/1-server-with-slow-function.js
@@ -1,29 +1,29 @@
-'use strict'
+"use strict";
 
-const express = require('express')
+const express = require("express");
 
-const app = express()
-const payload = initPayload()
+const app = express();
+const payload = initPayload();
 
-app.get('/', (req, res) => {
-  res.send(payload())
-})
+app.get("/", (req, res) => {
+  res.send(payload());
+});
 
-app.listen(3000)
+app.listen(3000, () => console.log("Server is running on port: 3000..."));
 
-function initPayload (idSize = 20) {
-  if (idSize < 6) throw Error('idSize must be greater than 5')
-  return function payload () {
-    let chars = ''
-    let n = idSize
-    const date = Date.now()
-    const radix = 36
-    n *= n * idSize
+function initPayload(idSize = 20) {
+  if (idSize < 6) throw Error("idSize must be greater than 5");
+  return function payload() {
+    let chars = "";
+    let n = idSize;
+    const date = Date.now();
+    const radix = 36;
+    n *= n * idSize;
     while (n--) {
-      const num = date + n
-      chars += num.toString(radix).toUpperCase()
+      const num = date + n;
+      chars += num.toString(radix).toUpperCase();
     }
-    const id = chars.slice(-idSize)
-    return { date, id }
-  }
+    const id = chars.slice(-idSize);
+    return { date, id };
+  };
 }
